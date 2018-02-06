05cb9c68e694f40cee7ba0b4c2d7caee8ad76373
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-06 08:23:56 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+
+class Anagram
+  @input = ""
+  @output = []
+  def initialize(input)
+    @input =  input
+  end
+
+  def match(words)
+    words.select{|word| word.chars.sort.join == @input.chars.sort.join}
+  end
+
+end
